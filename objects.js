adc95cf48f29c78b0e7d1a69c62c6a8d2b4b34c7
jill zhang <talktojillzhang@gmail.com>
2018-08-31 00:28:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
-var playlist =new Object()
+var playlist =new Object({"artist":"song"})
 
+function updatePlaylist(playlist,artistname,songtitle){
+   playlist[artistname] = songtitle;
+   return playlist;
+}
 function updatePlaylist(playlist,artistname,songtitle){
    playlist[artistname] = songtitle;
    return playlist;
